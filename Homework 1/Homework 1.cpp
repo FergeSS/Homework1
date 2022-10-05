da226c574c123d98c2847c8dad7a890c0fe15849
FergeSS <97806343+FergeSS@users.noreply.github.com>
2022-10-06 02:08:32 +0300

    Add files via upload

---
+++ Homework 1/Homework 1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include "Tasks.h"
+
+int main(){
+    int flag;
+
+    std::cout << "Choose Task (1, 2 or 3): ";
+    std::cin >> flag;
+    
+    while (flag != 1 && flag != 2 && flag != 3) {
+        system("cls");
+        std::cout << "Inccorect." << " Choose Task (1, 2 or 3): ";
+        std::cin >> flag;
+    }
+    if (flag == 1) {
+        system("cls");
+        task1();
+    }
+    if (flag == 2) {
+        system("cls");
+        task2();
+    }
+    if (flag == 3) {
+        system("cls");
+        task3();
+    }
+
+}
+
