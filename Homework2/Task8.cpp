3cf5588bd54c170ec0154d11646d6112e5ecfdf9
FergeSS <97806343+FergeSS@users.noreply.github.com>
2022-10-11 21:36:59 +0300

    Исправлены ошибки

--- Homework2/Task8.cpp
+++ Homework2/Task8.cpp
@@ -14,12 +14,12 @@ double my_pow( double digit,  int pow) {
 int task8() {
 	int w, h;
 
-	for (h = 21; h >= 1; h--) {
-		for (w = 41; w >= 1; w--) {
+	for (h = 20; h >= 0; h--) {
+		for (w = 40; w >= 0; w--) {
 			double x, y;
 
-			x = 3 * double(w)/ 41 - 1.5;
-			y = 3 * double(h) / 21 - 1.5;
+			x = 3 * double(w)/ 40 - 1.5;
+			y = 3 * double(h) / 20 - 1.5;
 
 			if (my_pow((my_pow(x, 2) + my_pow(y , 2) - 1), 3) - my_pow(x, 2) * my_pow(y, 3) <= 0)
 				std::cout << "*";
